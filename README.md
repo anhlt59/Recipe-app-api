1a2e3c33fe44bc2320e3d85ba65d1f258e518450
anhlt59 <tuananh220695@gmail.com>
2019-12-05 20:14:28 +0700

    update recipe

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # recipe-app-api
 Recipe app api source code
+
+# note
+if docker-compose.yml or Dockerfile is changed,
+	retyping "docker-compose build"
+	and "docker-compose up -d" to run in deamon
+
+docker-compose --rm > for services run once, avoid to linger on a system after it's ran
--- app/app/urls.py
+++ app/app/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
+    # path('docs/', get_swagger_view(title='Profile API')),
+    path('docs/', include_docs_urls(title='Profile API')),
 ]
